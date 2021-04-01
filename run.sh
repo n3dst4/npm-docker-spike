edab242e46990bf6f3f29b553337a00c6c917aad
Neil de Carteret <ndecarteret@sportingindex.com>
2021-04-01 11:18:42 +0100

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-rm -rf tmpmnt
-cp -a src tmpmnt
-docker run -it --volume=/home/ndc/git/trade-ui-build-spike/tmpmnt:/root tradespike
+rel_dir=$(dirname $0)
+directory=$(realpath $rel_dir)
+tmp=tmp
+src=src
+
+cd $directory
+rm -rf ${tmp}
+cp -a $src $tmp
+docker run -it --volume=${directory}/${tmp}:/${src} node:14 bash
