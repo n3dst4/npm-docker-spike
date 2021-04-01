d10b3d95a99a677017e5550329c530626301135e
Neil de Carteret <ndecarteret@sportingindex.com>
2021-04-01 14:38:08 +0100

    tweaks

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+TIMEFORMAT=%E
+
 function install {
-  npm i 2>&1 >/dev/null
+  echo $(npm i --no-progress --silent 2>&1 > /dev/null)
 }
 
 function time_install {
-  for n in {1..5}; do 
-    echo "$(pwd), npm $(npm --version), run ${n}"
+  for n in {1..3}; do 
+    echo -n "$(pwd), npm $(npm --version), run ${n}... "
     rm -rf node_modules package-lock.json
     time install
   done
@@ -20,8 +22,8 @@ time_install
 cd /local
 time_install
 
-echo "UPGRADING TO NPM 7"
-npm i -g npm@latest 2>&1 >/dev/null
+echo -n "UPGRADING TO NPM LATEST... "
+time npm i -g npm@latest --no-progress --silent 2>&1 >/dev/null
 
 cd /mounted
 time_install
